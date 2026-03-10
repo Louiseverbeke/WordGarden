ce5dc3d359ed414e77fc52f62a9f440ff7afb826
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-10 14:18:08 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Louise Verbeke on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
